19f4ecff5842a72825d3cb8af96f2f8375ba509d
Kostya <k.konstantin2212@gmail.com>
2024-09-12 12:36:06 +0300

    main

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+import './App.css';
+import Page from './page';
+const tg = window.Telegram.WebApp
+
+
+
+function App() {
+  return (
+    <div>
+      <Page />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/page/index.jsx
@@ -0,0 +1,147 @@
+import React from 'react';
+import './index.css';
+
+export default function Main() {
+  return (
+    <div className='main-container'>
+      <div className='flex-row-ba'>
+        <div className='rectangle'>
+          <button className='rectangle-1'>
+            <span className='wallet'>Wallet</span>
+          </button>
+          <span className='contest'>Contest</span>
+        </div>
+        <div className='regroup'>
+          <button className='group' />
+          <button className='vector' />
+        </div>
+        <button className='icon' />
+      </div>
+      <div className='rectangle-2'>
+        <div className='flex-row-c'>
+          <div className='wallet-c-account'>
+            <span className='wallet-c'>Wallet C </span>
+            <span className='empty'> </span>
+            <span className='account'>Account </span>
+          </div>
+          <div className='vector-3' />
+          <div className='image' />
+        </div>
+        <div className='flex-row-fe'>
+          <span className='comma'>7830000,930</span>
+          <span className='dollar'>$</span>
+        </div>
+      </div>
+      <div className='flex-row-a'>
+        <button className='rectangle-4'>
+          <div className='ellipse' />
+          <span className='receive'>Receive</span>
+          <div className='icon-5' />
+        </button>
+        <button className='rectangle-6'>
+          <div className='ellipse-7' />
+          <span className='send'>Send</span>
+          <div className='icon-8' />
+        </button>
+        <button className='rectangle-9'>
+          <div className='ellipse-a' />
+          <span className='swap'>Swap</span>
+          <div className='vector-b' />
+        </button>
+      </div>
+      <div className='dekor'>
+        <span className='crypto'>Crypto</span>
+      </div>
+      <div className='flex-row-e'>
+        <span className='eth'>ETH</span>
+        <span className='dot'>0.393039</span>
+        <div className='picture-eth' />
+        <span className='dollar-c'>$2,784.59</span>
+        <span className='percentage'>+0.63%</span>
+        <span className='dollar-d'>$273,093</span>
+      </div>
+      <div className='flex-row-ec'>
+        <span className='text-11'>USDT</span>
+        <span className='text-12'>50.6797</span>
+        <div className='img-7' />
+        <span className='text-13'>$1.0037</span>
+        <span className='text-14'>+0.63%</span>
+        <span className='text-15'>$61,768</span>
+      </div>
+      <div className='wrapper-6'>
+        <span className='text-16'>7.238</span>
+        <div className='wrapper-7'>
+          <div className='img-8' />
+          <span className='text-17'>USDC</span>
+        </div>
+      </div>
+      <div className='box-2'>
+        <span className='text-18'>$69.45</span>
+        <div className='box-3'>
+          <span className='text-19'>$0.9995</span>
+          <span className='text-1a'>-0.02%</span>
+        </div>
+      </div>
+      <div className='box-4'>
+        <span className='text-1b'>ETH</span>
+        <span className='text-1c'>0.393039</span>
+        <div className='pic-7' />
+        <span className='text-1d'>$2,784.59</span>
+        <span className='text-1e'>+0.63%</span>
+        <span className='span-dollar'>$273,093</span>
+      </div>
+      <div className='div-flex-row-b'>
+        <span className='span-usdt'>USDT</span>
+        <span className='span-dot'>50.6797</span>
+        <div className='div-picture-type-png' />
+        <span className='span-dollar-e'>$1.0037</span>
+        <span className='span-percent'>+0.63%</span>
+        <span className='span-dollar-f'>$61,768</span>
+      </div>
+      <div className='div-flex-row-d'>
+        <span className='span-dot-10'>7.238</span>
+        <div className='div-regroup'>
+          <div className='div-picture-usdc' />
+          <span className='span-usdc'>USDC</span>
+        </div>
+      </div>
+      <div className='div-flex-row-eaa'>
+        <span className='span-dollar-11'>$69.45</span>
+        <div className='div-regroup-12'>
+          <span className='span-dollar-13'>$0.9995</span>
+          <span className='span-percent-negative'>-0.02%</span>
+        </div>
+      </div>
+      <div className='div-flex-row'>
+        <span className='span-eth'>ETH</span>
+        <span className='span-dot-14'>0.393039</span>
+        <div className='div-picture-eth' />
+        <span className='dollar-amount'>$2,784.59</span>
+        <span className='percentage-15'>+0.63%</span>
+        <span className='dollar-amount-16'>$273,093</span>
+      </div>
+      <div className='flex-row-a-17'>
+        <span className='currency'>USDT</span>
+        <span className='decimal-number'>50.6797</span>
+        <div className='picture-type-png' />
+        <span className='dollar-amount-18'>$1.0037</span>
+        <span className='percentage-19'>+0.63%</span>
+        <span className='dollar-amount-1a'>$61,768</span>
+      </div>
+      <div className='flex-row-c-1b'>
+        <span className='decimal-number-1c'>7.238</span>
+        <div className='regroup-1d'>
+          <div className='picture-usdc' />
+          <span className='currency-1e'>USDC</span>
+        </div>
+      </div>
+      <div className='flex-row-c-1f'>
+        <span className='dollar-amount-20'>$69.45</span>
+        <div className='regroup-21'>
+          <span className='dollar-amount-22'>$0.9995</span>
+          <span className='negative-percentage'>-0.02%</span>
+        </div>
+      </div>
+    </div>
+  );
+}
